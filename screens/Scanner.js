cd0d5510d3cb2eb36fa14e28e6ca57b7aa784e32
digitalmarket <digitalmarket314@gmail.com>
2023-12-14 14:51:50 -0800

    add camera

--- screens/Scanner.js
+++ screens/Scanner.js
@@ -1,18 +1,13 @@
-import {
-  Alert,
-  Linking,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
 import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import { Button } from "react-native";
+import { Camera } from "expo-camera";
 
 const Scanner = () => {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
+  const [flashOn, setFlashOn] = useState(false);
 
   useEffect(() => {
     const getBarCodeScannerPermissions = async () => {
@@ -29,6 +24,19 @@ const Scanner = () => {
     console.log(data);
   };
 
+  const toggleFlash = async () => {
+    console.log("nil");
+    if (hasPermission) {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      console.log(status);
+      if (status === "granted") {
+        setFlashOn((prev) => !prev);
+      } else {
+        alert("Camera permission is required to toggle the flashlight.");
+      }
+    }
+  };
+
   if (hasPermission === null) {
     return <Text>Requesting for camera permission</Text>;
   }
@@ -41,7 +49,21 @@ const Scanner = () => {
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={StyleSheet.absoluteFillObject}
+        torchMode={
+          flashOn
+            ? Camera.Constants.FlashMode.torch
+            : Camera.Constants.FlashMode.off
+        }
       />
+
+      <View style={styles.overlay}>
+        <View style={styles.scanBox} />
+      </View>
+      <TouchableOpacity style={styles.flashButton} onPress={toggleFlash}>
+        <Text style={styles.flashButtonText}>
+          {flashOn ? "Turn Flash Off" : "Turn Flash On"}
+        </Text>
+      </TouchableOpacity>
       {scanned && (
         <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
       )}
@@ -49,11 +71,38 @@ const Scanner = () => {
   );
 };
 
+const overlayColor = "rgba(0,0,0,0.5)";
+const borderColor = "#FFF";
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
     justifyContent: "center",
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  scanBox: {
+    width: 200,
+    height: 200,
+    borderWidth: 2,
+    borderColor,
+    borderRadius: 10,
+    backgroundColor: "transparent",
+  },
+  flashButton: {
+    position: "absolute",
+    bottom: 20,
+    alignSelf: "center",
+    padding: 10,
+    backgroundColor: "black",
+    borderRadius: 5,
+  },
+  flashButtonText: {
+    color: "white",
   },
 });
 
